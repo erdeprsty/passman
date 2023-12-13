21dce76c496c346c30c45adff446016389f76808
Ardi Hari Prasetiyo <ardihariprasetiyoo@gmail.com>
2023-12-13 12:55:39 +0700

    fix: use sys argv instead of input

--- passman.py
+++ passman.py
@@ -1,18 +1,33 @@
 from hashlib import pbkdf2_hmac, sha256
 import base64
 import hmac
+import sys
+
+if len(sys.argv) < 2:
+    print('Master key argument is required')
+    exit()
+if len(sys.argv) < 3:
+    print('Platform credential argument is required')
+    exit()
+if not sys.argv[1]:
+    print('Invalid master key')
+    exit()
+if not sys.argv[2]:
+    print('Invalid platform credential')
+    exit()
+
+master_key = sys.argv[1]
+credential = sys.argv[2].split('@')
 
 PLATFORMS = {'google': {
     'ardihariprasetiyooo': 'rLwJxOcYkzSW9/0tciae693WFQePUoMcgvtECNrhsdc='}}
 SERCRET_KEY = '4mefIs6EkILQCACfojqfru2QczGxPlc4oOytQoPivG0='
-master_key = input('Master key: ')
 master_key_digest = base64.b64encode(hmac.new(bytes(
     master_key, encoding='utf-8'), b'masterkey', sha256).digest()).decode('utf-8')
 if master_key_digest != SERCRET_KEY:
     print('Invalid master key')
     exit()
 
-credential = input('platform@account: ').split('@')
 credential_platform = credential[0]
 credential_account = len(credential) > 1 and credential[1] or None
 if credential_platform not in PLATFORMS.keys():
